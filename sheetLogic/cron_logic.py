2f7c81f59a9cbcae49d83e7bd3ea1bec274c0d05
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-04 15:43:43 -0400

    Adding test cron logic for sending out messages

---
+++ sheetLogic/cron_logic.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+from typing import Dict, List
+
+from sheetLogic.user_sheet import UserSheet
+
+
+class CronLogic(UserSheet):
+    def __init__(self):
+        super().__init__()
+
+    def users(self):
+        users: Dict[str, List[str]] = defaultdict(list)
+        for tier in self.tiers:
+            for user in self.sheet:
+                if user[1] == tier:
+                    users.get(tier).append(user[1])
+        return users
---
+++ telegramLogic/test_cron.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+from telegram import Update
+from telegram.ext import CallbackContext
+
+from send_typing_action import send_typing_action
+from sheetLogic.cron_logic import CronLogic
+from sheetLogic.tier_logic import TierLogic
+
+
+@send_typing_action
+def test_cron(update: Update, context: CallbackContext):
+    tier_logic = TierLogic()
+    cron_logic = CronLogic()
+
+    data = tier_logic.get_today_data()
+    users = cron_logic.users()
+
+    for tier, message in data.items():
+        for user_id in users.get(tier):
+            context.bot.send_message(user_id, f'{date.today()}\n\n{message}')
