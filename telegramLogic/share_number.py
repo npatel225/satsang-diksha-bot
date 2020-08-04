319b1ea03fd86d7798ec1326089f16b139cc841e
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-03 23:58:04 -0400

    Fixing share_number.py logic

--- telegramLogic/share_number.py
+++ telegramLogic/share_number.py
@@ -8,23 +8,11 @@ from send_typing_action import send_typing_action
 
 @send_typing_action
 def share_number(update: Update, context: CallbackContext):
-    query: CallbackQuery = update.callback_query
-    message: Message = query.message
-    region = query.data
-
-    context.user_data.update({'region': region})
-
     contact_keyboard = KeyboardButton(text="send_contact", request_contact=True)
     custom_keyboard = [[contact_keyboard]]
     reply_markup = ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True)
 
-    t1 = threading.Thread(target=lambda: context.bot.delete_message(message.chat_id, message.message_id))
-    t1.start()
-
-    t2 = threading.Thread(target=lambda: message.reply_text('Please share your contact information with us (required)',
-                                                            reply_markup=reply_markup))
-    t2.start()
-    t1.join()
-    t2.join()
+    update.message.reply_text.reply_text('Please share your contact information with us (required)',
+                                         reply_markup=reply_markup)
 
     return '/choose_tier'
