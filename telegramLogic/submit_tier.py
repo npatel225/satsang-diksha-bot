fa30415b6753d654e557d26d605843f57ce0a148
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-04 00:16:13 -0400

    Print internal ID

--- telegramLogic/submit_tier.py
+++ telegramLogic/submit_tier.py
@@ -12,9 +12,8 @@ def submit_tier(update: Update, context: CallbackContext):
     context.user_data.update({'tier': tier})
     phone_number = context.user_data.get('phone_number', '')
 
-    info = f'Phone Number: {phone_number}, Tier: {tier}'
+    info = f'Phone Number: {phone_number}, Tier: {tier}, User\'s Internal ID: {update.message.chat_id}'
 
     query.edit_message_text(text=info)
 
     return ConversationHandler.END
-
