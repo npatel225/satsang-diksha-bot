87e77f03b627ab9fae47676f98e25f3ff9dd6d43
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-04 00:10:45 -0400

    update submit_tier.py dictionary properly

--- telegramLogic/submit_tier.py
+++ telegramLogic/submit_tier.py
@@ -9,7 +9,7 @@ def submit_tier(update: Update, context: CallbackContext):
     query = update.callback_query
 
     tier = query.data
-    context.user_data.update({'tier', tier})
+    context.user_data.update({'tier': tier})
     phone_number = context.user_data.get('phone_number', '')
 
     info = f'Phone Number: {phone_number}, Tier: {tier}'
