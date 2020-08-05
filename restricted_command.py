ff093626ac889c9c3d137707039af6b2469e0aed
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-05 11:21:13 -0400

    Adding restricted_command.py wrap

---
+++ restricted_command.py
@@ -0,0 +1,15 @@
+from functools import wraps
+
+LIST_OF_ADMINS = {263366770, 89505043}
+
+
+def restricted_command(func):
+    @wraps(func)
+    def wrapped(update, context, *args, **kwargs):
+        user_id = update.effective_user.id
+        if user_id not in LIST_OF_ADMINS:
+            print("Unauthorized access denied for {}.".format(user_id))
+            return
+        return func(update, context, *args, **kwargs)
+
+    return wrapped
