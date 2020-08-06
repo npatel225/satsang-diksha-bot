ca6d119507ff896ac8c65e4a552296355e41f0a8
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-05 21:19:45 -0400

    daily_diksha_message.py uses threading now

--- cronLogic/daily_diksha_message.py
+++ cronLogic/daily_diksha_message.py
@@ -1,5 +1,6 @@
 import os
 from datetime import date
+from threading import Thread
 
 from telegram import Bot
 
@@ -15,10 +16,14 @@ def daily_diksha_message():
 
     cron_logic = CronLogic()
     users = cron_logic.users()
-
+    threads = []
     for tier, messages in data.items():
         for user_id in users[tier]:
-            [bot.send_message(user_id, f'{date.today()}\n\n{message}') for message in messages]
+            for message in messages:
+                thread = Thread(target=lambda: bot.send_message(user_id, f'{date.today()}\n\n{message}'))
+                thread.start()
+                threads.append(thread)
+    [thread.join() for thread in threads]
 
 
 if __name__ == '__main__':
