06c1f91ecaabdd413568b2d6a9af87e224460250
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-09 14:29:57 -0400

    print

--- telegramLogic/main_message_handler.py
+++ telegramLogic/main_message_handler.py
@@ -1,6 +1,7 @@
 import telegram
 from telegram import Update, Message
 from telegram.ext import CallbackContext
+import re
 
 from sheetLogic.sheet_messages import SheetMessages
 
@@ -10,5 +11,8 @@ def main_message_handler(update: Update, context: CallbackContext):
     message.delete()
     message_dict = SheetMessages().message_dict()
 
+    # m = message_dict.get(message.text, "Please run `/start` again")
+    # if
+    print(message.text, message_dict.get(message.text, "Please run `/start` again"))
     message.reply_text(text=message_dict.get(message.text, "Please run `/start` again"),
                        parse_mode=telegram.ParseMode.MARKDOWN_V2)
