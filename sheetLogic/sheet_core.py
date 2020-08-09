73be5b5bbfd779c2c7111cf7e475c0f5c25142c5
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-09 16:09:40 -0400

    Storing credentials in a file and having a timeout

--- sheetLogic/sheet_core.py
+++ sheetLogic/sheet_core.py
@@ -1,8 +1,13 @@
 import functools
+import json
+import os
 from datetime import timedelta, datetime
 from functools import lru_cache
 
 import gspread
+from oauth2client.client import AccessTokenCredentials
+from oauth2client.service_account import ServiceAccountCredentials
+
 from security import Security
 from sheetLogic.sheet_config import SheetConfig
 
@@ -26,18 +31,41 @@ def cache(seconds: int, maxsize: int = 128, typed: bool = False):
     return wrapper_cache
 
 
+token_file = "./access_token.txt"  # token file including the authorization data
+now = int(datetime.now().timestamp())
+
+
 class SheetCore(SheetConfig):
     def __init__(self):
         super().__init__()
         self.spreadsheet = self.login.open_by_key(self.sheet_id)
 
     @property
-    @cache(100)
     def login(self):
         security = Security()
         security.decrypt_cred()
-        scopes = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file']
-        return gspread.service_account('./credentials.json', scopes=scopes)
+        credentials = self.getCredential()
+        return gspread.authorize(credentials)
+
+    def getNewAccessToken(self, credential_file):
+        scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive.file', ]
+        credentials = ServiceAccountCredentials.from_json_keyfile_name(credential_file, scope)
+        gc = gspread.authorize(credentials)
+        token_response = gc.auth.token_response
+        token_response['limitTime'] = token_response['expires_in'] + now - 300
+        with open(token_file, mode='w') as f:
+            json.dump(token_response, f)
+        return token_response['access_token']
+
+    def getCredential(self):
+        if os.path.exists(token_file):
+            with open(token_file) as f:
+                token = json.load(f)
+            access_token = token['access_token'] if token['limitTime'] > now else self.getNewAccessToken(
+                './credentials.json')
+        else:
+            access_token = self.getNewAccessToken('./credentials.json')
+        return AccessTokenCredentials(access_token, None)
 
     def get_sheet(self, title: str):
         return self.spreadsheet.worksheet(title=title)
