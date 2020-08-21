a34ae508db34cc1bc30fb8b0a58f2d849e2dbd3c
jayp0521 <jay.r.patel2115@gmail.com>
2020-08-21 12:06:54 -0400

    challenge_logic.py using map instead of another for loop

--- sheetLogic/challenge_logic.py
+++ sheetLogic/challenge_logic.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
 from datetime import datetime, date, timedelta
+from functools import partial
 from typing import Dict, List, Tuple
 from gspread import Worksheet
 
@@ -15,21 +16,27 @@ class ChallengeLogic(SheetCore):
     def get_sheets(self):
         return {tier: self.get_sheet(tier) for tier in self.tiers}
 
+    @staticmethod
+    def __challenge_message_parse(data, hour_delta, today_data, challenge):
+        try:
+            given_datetime = datetime.strptime(data[MessageEnum.DATE.value], '%m/%d/%Y') - timedelta(
+                hours=hour_delta)
+            if given_datetime.date() == date.today():
+                today_data[challenge].append(
+                    (data[MessageEnum.VIDEO_LINK.value],
+                     data[MessageEnum.GRAPHIC_LINK.value],
+                     data[MessageEnum.MOTIVATIONAL_LINK.value],
+                     data[MessageEnum.MESSAGE.value],
+                     )
+                )
+        except ValueError:
+            pass
+
     def get_today_data(self, hour_delta=0) -> Dict[str, List[Tuple[str, str, str, str]]]:
         today_data: Dict[str, List[Tuple[str, str, str, str]]] = defaultdict(list)
         for challenge, tier_sheet in self.challenge_sheets.items():
-            for data in tier_sheet.get_all_values()[1:]:
-                try:
-                    given_datetime = datetime.strptime(data[MessageEnum.DATE.value], '%m/%d/%Y') - timedelta(
-                        hours=hour_delta)
-                    if given_datetime.date() == date.today():
-                        today_data[challenge].append(
-                            (data[MessageEnum.VIDEO_LINK.value],
-                             data[MessageEnum.GRAPHIC_LINK.value],
-                             data[MessageEnum.MOTIVATIONAL_LINK.value],
-                             data[MessageEnum.MESSAGE.value],
-                             )
-                        )
-                except ValueError:
-                    pass
+            parser = partial(self.__challenge_message_parse, hour_delta=hour_delta, today_data=today_data,
+                             challenge=challenge)
+            list(map(parser, tier_sheet.get_all_values()[1:]))
+
         return today_data
